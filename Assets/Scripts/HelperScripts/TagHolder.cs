9e14867aec2b9599d568a11da6a746a86164da17
Warao Gil <wgil14@gmail.com>
2020-07-15 19:57:57 -0400

    Add footsteps, sprint and crounch

--- Assets/Scripts/HelperScripts/TagHolder.cs
+++ Assets/Scripts/HelperScripts/TagHolder.cs
@@ -3,8 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class Axis
+public static class Axis
 {
     public const string HORIZONTAL = "Horizontal";
     public const string VERTICAL = "Vertical";
+}
+
+public static class MouseAxis
+{
+    public const string MOUSE_X = "Mouse X";
+    public const string MOUSE_Y = "Mouse Y";
 }

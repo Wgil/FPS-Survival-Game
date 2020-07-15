4ad8a19b6be0bc5df7ceda793c0f617576ed3bdb
Warao Gil <wgil14@gmail.com>
2020-07-15 17:24:24 -0400

    Player can walk and jump

---
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController character_Controller;
+
+    private Vector3 move_Direction;
+
+    public float speed = 5f;
+    private float gravity = 20f;
+
+    public float jump_Force = 10f;
+    private float vertical_Velocity;
+
+    private void Awake()
+    {
+        character_Controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveThePlayer();
+    }
+
+    void MoveThePlayer()
+    {
+        move_Direction = new Vector3(Input.GetAxis(Axis.HORIZONTAL), 0f, Input.GetAxis(Axis.VERTICAL));
+
+        // transform from local space to world space. It looks like it behaves the same without this line.
+        move_Direction = transform.TransformDirection(move_Direction);
+        move_Direction *= speed * Time.deltaTime;
+
+        ApplyGravity();
+
+        character_Controller.Move(move_Direction);
+    }
+
+    void ApplyGravity()
+    {
+        // What this does is stick the GameObject to the ground allowing isGrounded to be true.
+        // Without it, it would be in the air at same Y coordinate after going up a hill since there's no gravity
+        // it won't jump as well since vertical_Velocity starts at 0.
+        vertical_Velocity -= gravity * Time.deltaTime;
+
+        PlayerJump();
+        move_Direction.y = vertical_Velocity * Time.deltaTime;
+    }
+
+    void PlayerJump()
+    {
+        if (character_Controller.isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Grounded");
+            vertical_Velocity = jump_Force;
+        }
+    }
+}
